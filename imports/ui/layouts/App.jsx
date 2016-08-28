ef91c7706f56a587c57821bcaef7801581b36775
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2016-08-28 09:55:41 +0200

    Fixed missing class properties transform

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,11 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
 import { autobind } from 'core-decorators';
+import { createContainer } from 'meteor/react-meteor-data';
 
 import Button from 'react-bootstrap/lib/Button';
 
 
 class App extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object.isRequired,
+  }
+
+  @autobind
+  handleRouteToSite() {
+    this.context.router.push('/site')
+  };
 
   render() {
     return(
